14d8fdcb1c0277f162eb1e199de0ce7e2a0664da
Ubuntu <ubuntu@ip-172-31-44-100.ap-northeast-1.compute.internal>
2020-09-24 12:39:11 +0000

    add column

--- app/Http/Controllers/TasksController.php
+++ app/Http/Controllers/TasksController.php
@@ -42,6 +42,7 @@ class TasksController extends Controller
     public function store(Request $request)
     {
         $task = new Task;
+        $task->status = $request->status; 
         $task->content = $request->content;
         $task->save();
         
@@ -88,6 +89,7 @@ class TasksController extends Controller
      public function update(Request $request, $id)
     {
         $task = Task::findOrFail($id);
+        $task->status = $request->status;
         $task->content = $request->content;
         $task->save();
 
